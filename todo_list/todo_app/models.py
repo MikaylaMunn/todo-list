7da327349a5ee76e45c5206a70388f2f3d857f40
Mikayla Munn <mikayla.munn@gmail.com>
2022-11-29 20:26:20 -0500

    Adding in comments

--- todo_list/todo_app/models.py
+++ todo_list/todo_app/models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here.
 class Task(models.Model):
     description = models.CharField(max_length=255)
+class Comment(models.Model):
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    task = models.ForeignKey(Task, on_delete = models.CASCADE)
--- todo_list/todo_app/views.py
+++ todo_list/todo_app/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import redirect, render
 from django.views import View
 # Create your views here.
 
-from todo_app.models import Task
-from todo_app.forms import TaskForm
+from todo_app.models import Task, Comment
+from todo_app.forms import TaskForm, CommentForm
 
 class HomeView(View):
     def get(self, request):
@@ -28,9 +28,13 @@ class TaskDetailView(View):
     def get(self,request,task_id):
         task = Task.objects.get(id=task_id)
         task_form = TaskForm(instance=task)
+        comment = Comment.objects.filter(task_id = task_id)
+        comment_form = CommentForm(task_object = task)
         html_data = {
             'task': task,
             'form' : task_form,
+            'comment_list': comment,
+            'comment_form': comment_form
         }
         return render (
             request = request,
@@ -44,4 +48,8 @@ class TaskDetailView(View):
             task_form.save()
         elif 'delete' in request.POST:
             task.delete()
+        elif 'add' in request.POST:
+            comment_form = CommentForm(request.POST, task_object = task)
+            comment_form.save()
+            return redirect('task_detail', task.id)
         return redirect('home')
